912d63d00c97d5e8943bf335d9a8d0ad29042b33
fubao <freadream@gmail.com>
2017-12-04 22:40:01 -0500

    add_functions

--- files.py
+++ files.py
@@ -5,13 +5,11 @@ Created on Mon Dec  4 22:35:58 2017
 
 @author: fubao
 """
+
+import numpy as np
+
 from scipy import misc
 
-
-def read_scene():
-	data_x = misc.imread('../../Data/Scene/times_square.jpg')
-
-	return (data_x)
 
 def read_faces():
 	nFaces = 100
@@ -20,5 +18,11 @@ def read_faces():
 
 	for i in np.arange(nFaces):
 		data_x = np.vstack((data_x, np.reshape(misc.imread('../../Data/Faces/face_%s.png' % (i)), (1, nDims))))
+
+	return (data_x)
+
+
+def read_scene():
+	data_x = misc.imread('../../Data/Scene/times_square.jpg')
 
 	return (data_x)
