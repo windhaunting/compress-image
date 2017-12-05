94e99711b50d5ecec8c0b188fbbac08faaaece32
fubao <freadream@gmail.com>
2017-12-04 23:03:01 -0500

    add_functions

--- run_me.py
+++ run_me.py
@@ -1,52 +1,38 @@
 # Import modules
 
 import numpy as np
-from scipy import misc
 
-def read_scene():
-	data_x = misc.imread('../../Data/Scene/times_square.jpg')
-
-	return (data_x)
-
-def read_faces():
-	nFaces = 100
-	nDims = 2500
-	data_x = np.empty((0, nDims), dtype=float)
-
-	for i in np.arange(nFaces):
-		data_x = np.vstack((data_x, np.reshape(misc.imread('../../Data/Faces/face_%s.png' % (i)), (1, nDims))))
-
-	return (data_x)
+from files import read_faces
+from files import read_scene
 
 
-def getPCA():
-    X = 1
+def getPCAImage():
+    data_x = read_faces()
+    print('X = ', data_x.shape)
+
+    print('Implement PCA here ...')
+    
     
     
 if __name__ == '__main__':
 	
 	################################################
 	# PCA
-
-	data_x = read_faces()
-	print('X = ', data_x.shape)
-
-	print('Implement PCA here ...')
+    getPCAImage()
+	
 	
 	################################################
     
-    
-    
 	# K-Means
 
-	data_x = read_scene()
-	print('X = ', data_x.shape)
+    data_x = read_scene()
+    print('X = ', data_x.shape)
 
-	flattened_image = data_x.ravel().reshape(data_x.shape[0] * data_x.shape[1], data_x.shape[2])
-	print('Flattened image = ', flattened_image.shape)
+    flattened_image = data_x.ravel().reshape(data_x.shape[0] * data_x.shape[1], data_x.shape[2])
+    print('Flattened image = ', flattened_image.shape)
 
-	print('Implement k-means here ...')
+    print('Implement k-means here ...')
 
-	reconstructed_image = flattened_image.ravel().reshape(data_x.shape[0], data_x.shape[1], data_x.shape[2])
-	print('Reconstructed image = ', reconstructed_image.shape)
+    reconstructed_image = flattened_image.ravel().reshape(data_x.shape[0], data_x.shape[1], data_x.shape[2])
+    print('Reconstructed image = ', reconstructed_image.shape)
 
